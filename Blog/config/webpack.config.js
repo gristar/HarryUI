a850a6b697f5670b00853d972c4b5f957c322a4a
chens <chens@strongu.com.cn>
2018-11-02 14:43:52 +0800

    修改 webpack 配置

--- Blog/config/webpack.config.js
+++ Blog/config/webpack.config.js
@@ -7,6 +7,29 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/, use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {test: /\.css$/, use: 'css-loader'},
+            {test: /\.ts$/, use: 'ts-loader'},
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                }
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin(),
     ]
